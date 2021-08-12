b3ca173463b9d71170f642bfda0adffb377bb56d
Okpe onoja <67971490+onoja123@users.noreply.github.com>
2021-08-12 16:20:42 +0100

    Create index.js

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ output = [];
 count = 1;
 
 function fizzbuzz() {
- if ( count === 3 && count === 5){
+ if ( count % 3 && count % 5){
   output.push("FizzBuzz")
  }
- esle if (count === 3){
+ esle if (count % 3){
   output.push("fizz")
  }
- else if (count === 5){
+ else if (count % 5){
   output.push("fuzz")
  }else{
    output.push(count);
  }
  count++;
-
+  console.log(output);
 }
