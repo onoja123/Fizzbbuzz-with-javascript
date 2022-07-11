b5df4b088e31445c23dd38f13326a0d2d484b723
Okpe onoja <67971490+onoja123@users.noreply.github.com>
2022-07-11 17:26:32 +0100

    Added For loop

--- index.js
+++ index.js
@@ -16,13 +16,33 @@ let count = 1;
  }
 
 
-//Fizzbuzz using while
+//Fizzbuzz using while Loop
 
 let output = []
 let count = 1;
 
      while( count <= 100){
  function fizzbuzz(){
+    if(count % 3 === 0 && count % 5 === 0){
+        output.push("fizzbuzz")
+    }else if(count % 3 === 0){
+        output.push("fizz")
+    }else if(count % 5){
+        output.push("buzz")
+    }else{
+        count ++
+    }
+    output.push(count)
+    }
+}
+
+
+let output = []
+let count = 1;
+
+// Fizzbuzz using For Loop     
+ function fizzbuzz(){
+  for(i =0; i < 100; i++){
     if(count % 3 === 0 && count % 5 === 0){
         output.push("fizzbuzz")
     }else if(count % 3 === 0){
