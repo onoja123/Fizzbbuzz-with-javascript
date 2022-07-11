40c13f0945bb53c1eabe58714b218d32994e69f2
Okpe onoja <67971490+onoja123@users.noreply.github.com>
2022-07-11 15:45:49 +0100

    Added fizzbuzz using while loop

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-output = [];
-count = 1;
+// Fizzbuzz using js
+let output = [}
+let count = 1;
 
-function fizzbuzz() {
- if ( count % 3 && count % 5){
-  output.push("FizzBuzz")
+ function fizzbuzz(){
+    if(count % 3 === 0 && count % 5 === 0){
+        output.push("fizzbuzz")
+    }else if(count % 3 === 0){
+        output.push("fizz")
+    }else if(count % 5){
+        output.push("buzz")
+    }else{
+        count ++
+    }
+    output.push(count)
  }
- esle if (count % 3){
-  output.push("fizz")
- }
- else if (count % 5){
-  output.push("fuzz")
- }else{
-   output.push(count);
- }
- count++;
-  console.log(output);
+
+
+//Fizzbuzz using while
+
+let output = []
+let count = 1;
+
+     while( count <= 100){
+ function fizzbuzz(){
+    if(count % 3 === 0 && count % 5 === 0){
+        output.push("fizzbuzz")
+    }else if(count % 3 === 0){
+        output.push("fizz")
+    }else if(count % 5){
+        output.push("buzz")
+    }else{
+        count ++
+    }
+    output.push(count)
+    }
 }
